755b7f24e42e81d4e4e5f727306d52a17b33fdfa
Kris Burke <kris.burke611@gmail.com>
2017-07-26 15:11:50 -0400

    Finish svm mini project

--- svm/svm_author_id.py
+++ svm/svm_author_id.py
@@ -1,17 +1,19 @@
 #!/usr/bin/python
 
-""" 
+"""
     This is the code to accompany the Lesson 2 (SVM) mini-project.
 
-    Use a SVM to identify emails from the Enron corpus by their authors:    
+    Use a SVM to identify emails from the Enron corpus by their authors:
     Sara has label 0
     Chris has label 1
 """
-    
+
 import sys
 from time import time
-sys.path.append("../tools/")
+sys.path.append("/Users/krisburke/Learn/Udacity/ud120-projects/tools")
 from email_preprocess import preprocess
+from sklearn.svm import SVC
+from sklearn.metrics import accuracy_score
 
 
 ### features_train and features_test are the features for the training
@@ -19,12 +21,30 @@ from email_preprocess import preprocess
 ### labels_train and labels_test are the corresponding item labels
 features_train, features_test, labels_train, labels_test = preprocess()
 
-
-
+# uncomment below for smaller training set
+# features_train = features_train[:len(features_train)/100]
+# labels_train = labels_train[:len(labels_train)/100]
 
 #########################################################
 ### your code goes here ###
 
 #########################################################
 
+# clf = SVC(kernel="linear")
+clf = SVC(kernel="rbf", C=10000.)
+print "c is 10000"
 
+t0 = time()
+clf.fit(features_train, labels_train)
+print "training time:", round(time()-t0, 3), "s"
+
+t1 = time()
+pred = clf.predict(features_test)
+print "prediction time:", round(time()-t1, 3), "s"
+
+accuracy = accuracy_score(labels_test, pred)
+print "Accuracy: ", accuracy
+
+print "Answer 10,26,50 ", pred[10], pred[26], pred[50]
+
+print "no. of Chris emails predicted: ", sum(pred) # this works because Chris = 1, Sara = 0
